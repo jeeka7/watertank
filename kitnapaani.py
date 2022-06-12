12a5ee65030b331ce74c0963f578ecc46e07a997
Dean Hunter <jeeka7@gmail.com>
2022-06-12 23:37:50 +0530

    Create kitnapaani.py

---
+++ kitnapaani.py
@@ -0,0 +1,11 @@
+st.write("Enter the Time taken by each pipe in same units")
+
+pipe1 = st.slider("Pipe 1",0,30,0)
+pipe2 = st.slider("Pipe 2",0,30,0)
+pipe3 = st.slider("Pipe 3",0,30,0)
+
+pipe1_2 = st.slider("Pipe 1 and 2",0,30,0)
+pipe2_3 = st.slider("Pipe 2 and 3",0,30,0)
+pipe3_1 = st.slider("Pipe 1 and 3",0,30,0)
+
+pipe1_2_3 = st.slider("All 3 pipes",0,30,0)
