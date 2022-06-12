3392f60292af6e3a49843d17c53d07adc42930e7
Dean Hunter <jeeka7@gmail.com>
2022-06-13 00:19:00 +0530

    Update kitnapaani.py

--- kitnapaani.py
+++ kitnapaani.py
@@ -16,3 +16,24 @@ pipe1_2_3 = st.slider("All 3 pipes",0,30,0)
 work = math.lcm(pipe1,pipe2,pipe3,pipe1_2,pipe2_3,pipe3_1)
 
 #st.write("capacity of tank is ", work)
+
+e1 = work / pipe1
+
+e2 = work / pipe2
+
+e3 = work / pipe3
+
+e1_2 = work / pipe1_2
+e2_3 = work / pipe2_3
+e1_3 = work / pipe3_1
+
+e123 = work / pipe1_2_3
+
+e123 = e1 + e2 + e3
+e1_2 = e1 + e2
+e2_3 = e2 + e3
+e3_1 = e3 + e1
+
+pipe1 = work / e1
+pipe2 = work / e2
+pipe = work / e3
