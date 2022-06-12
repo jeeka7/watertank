36c2dfa86768c0f7e7f8d163853fb3bf6cbaa9b0
Dean Hunter <jeeka7@gmail.com>
2022-06-12 23:50:24 +0530

    Update kitnapaani.py

--- kitnapaani.py
+++ kitnapaani.py
@@ -12,3 +12,7 @@ pipe2_3 = st.slider("Pipe 2 and 3",0,30,0)
 pipe3_1 = st.slider("Pipe 1 and 3",0,30,0)
 
 pipe1_2_3 = st.slider("All 3 pipes",0,30,0)
+
+work = math.lcm(pipe1,pipe2,pipe3,pipe1_2,pipe2_3,pipe3_1)
+
+st.write("capacity of tank is ")
