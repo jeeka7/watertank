e12698a748246945b32eed4b77608aa10ecc22a8
Dean Hunter <jeeka7@gmail.com>
2022-06-13 04:17:01 +0530

    Update kitnapaani.py

--- kitnapaani.py
+++ kitnapaani.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import math
+from math import gcd
 
 st.write("Enter the Time taken by each pipe in same units")
 
@@ -13,17 +14,28 @@ pipe3_1 = st.slider("Pipe 1 and 3",0,30,0)
 
 pipe1_2_3 = st.slider("All 3 pipes",0,30,0)
 
-work = math.lcm(pipe1,pipe2,pipe3,pipe1_2,pipe2_3,pipe3_1)
-
-st.write("capacity of tank is ", work)
+#work = math.lcm(pipe1,pipe2,pipe3,pipe1_2,pipe2_3,pipe3_1)
 
 time_taken = [pipe1,pipe2,pipe3,pipe1_2,pipe2_3,pipe3_1,pipe1_2_3]
 
-st.write("original tome taken is",time_taken)
+NonZeroPipes = []
 
-from math import gcd
-NonZeroPipes = []  
-lcm = list1[0]
-for i in list1[1:]:
+def RemoveZeroes():
+  for pipe in range time_taken:
+    if pipe == 0;
+      pass
+    else:
+      NonZeroPipes.append(pipe)
+
+RemoveZeroes()
+def PrintKnownPipes():
+  for pipe in range time_taken:
+    st.write(pipe)
+
+PrintKnownPipes()    
+    
+lcm = NonZeroPipes[0]
+for i in NonZeroPipes[1:]:
   lcm = int(lcm*i/gcd(lcm, i))
-st.write("least common multiple =  ", lcm)
+  
+st.write("capacity of tank is ", lcm)
